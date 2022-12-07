b006053b3d1375ce1a6060cc44a9abb673a2b6a9
Mariam <mariam.aziz2014@hotmail.com>
2022-12-07 10:50:04 -0600

    Added Comments

--- src/BruteForce.java
+++ src/BruteForce.java
@@ -1,17 +1,25 @@
+// imports a Bcrypt package to implement the BCrypt hash
 import org.mindrot.BCrypt;
-
+// imports java classes to accomodate different hashing algorithms (MD5, SHA-256, BCrypt)
 import java.io.*;
 import java.security.NoSuchAlgorithmException;
 import java.util.*;
+
+// class that implements a brute force attack 
 public class BruteForce {
+
+        // all possible characters that can be cracked using our algorithm
         private static String[] possibleChars = {
                 "0", "1", "2", "3","4","5","6","7", "8","9",
                 "a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z",
                 "A","B","C","D", "E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z",
                 "~", "`", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "-", "+", "=", "{", "[", "}", "]", "|", "\\", ":", ";", "\"", "'", "<", ",", ">", ".", "?", "/" };
 
+        // declares and initializes a password guess string array with 5 characters 
         private static String[] currentGuess = {"0", "0", "0", "0", "0"};
 
+        // loops through every possible combination of characters
+        // returns correct guess or an error message
         public static String changeChar(int hashType, String hashedPW, String salt) throws NoSuchAlgorithmException {
             int currentIndex = 0;
             for (int h = 0; h<possibleChars.length; h++){
@@ -30,7 +38,7 @@ public class BruteForce {
                                 currentIndex = 4;
                                 currentGuess[currentIndex] = possibleChars[l];
 
-                                if (bruteCompare(currentGuess, hashType, hashedPW, salt)) {
+                                if (bruteCompare(currentGuess, hashType, hashedPW, salt)) { // calls method to check the guess against the hashed password
                                     String guess = "";
                                     for (int m = 0; m < currentGuess.length; m++) {
                                         guess += currentGuess[m];
--- src/PasswordCrackerClass.java
+++ src/PasswordCrackerClass.java
@@ -1,28 +1,39 @@
+// imports a Bcrypt package to implement the BCrypt hash
 import org.mindrot.BCrypt;
-
+// imports java classes to accomodate different hashing algorithms (MD5, SHA-256, BCrypt)
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.security.NoSuchAlgorithmException;
 import java.util.*;
 
+// main runner class
 public class PasswordCrackerClass
 {
+    // main method that runs our password cracking algorithms
     public static void main (String[] args) throws FileNotFoundException, NoSuchAlgorithmException {
+        //declares and initializes local variables
         String salt = BCrypt.gensalt();
         int attackType;
         String userPW;
         String unhashedPW = "";
         int hashType = 0;
         int input;
-        Scanner in = new Scanner (System.in);
-        userPW=args[0];
-        attackType = Integer.parseInt(args[1]);
-        hashType = Integer.parseInt(args[2]);
-        input = Integer.parseInt(args[3]);
+
+        // takes in user password, attack type, hash type, and input type as command line arguments
+        // user password is a string
+        // attack type, hash type, and input type are integers
+        userPW = args[0];
+        attackType = Integer.parseInt(args[1]); // 1 (Dictionary) or 2 (Bruteforce)
+        hashType = Integer.parseInt(args[2]); // 1 (MD5) or 2 (SHA-256) or 3 (BCrypt)
+        input = Integer.parseInt(args[3]); // 1 (plain text) or 2 (hash)
+        
+        // if there is a 5th argument, assign it as the salt
         if (args.length==5){
             salt = args[4];
         }
-            if (input==1){
+
+            // if password is in plain text, hash according to the specified hash type
+            if (input == 1){
                 if (hashType == 1){
                     userPW = MD5Hash.getMd5(userPW);
                 }
@@ -34,6 +45,7 @@ public class PasswordCrackerClass
                 }
             }
 
+        // runs attacks
         if (attackType == 1)
         {
             if(DictionaryAttack.dictionaryCrack(userPW, hashType, salt).equals("Is a Match!")){
